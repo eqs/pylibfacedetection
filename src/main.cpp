6c65a12ed4e6a1b0b6b895aa5e00ff1131fcabe6
eqs <murashige.satoshi.mi1@is.naist.jp>
2019-11-02 03:58:37 +0900

    Add main cpp file

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+int add(int i, int j) {
+	return i + j;
+}
+
+PYBIND11_MODULE(libfacedetection_wrapper, m) {
+	m.doc() = "Python binding of libfacedetection";
+
+	m.def("add", &add, "example function");
+}
