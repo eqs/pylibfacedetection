b1f328722b1429a19ba1fff0ba73141967e55443
eqs <murashige.satoshi.mi1@is.naist.jp>
2019-11-02 16:26:48 +0900

    wrap backend

--- pylibfacedetection/__init__.py
+++ pylibfacedetection/__init__.py
@@ -1,5 +1,7 @@
 """pylibfacedetection - Python binding of libfacedetection"""
 
+from .backend.clibfacedetection import FaceDetector
+
 __version__ = '0.0.1'
 __author__ = 'eqs <murashige.satoshi.mi1 [at] is.naist.jp>'
-__all__ = []
+__all__ = ['FaceDetector']
